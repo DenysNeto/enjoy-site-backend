53ee9c41dc408b524f8354bcd06382236b7db05a
DenysNeto <46863519+DenysNeto@users.noreply.github.com>
2024-05-26 22:17:24 +0300

    added func for webstudio

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ const formidable = require("formidable");
 const TelegramBot = require("node-telegram-bot-api");
 
 // Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather
-const bot = new TelegramBot("6901768703:AAFVfxlL81H5t7fQLlQ_lDfD0A7jwNNeD24", {
+
+//6901768703:AAFVfxlL81H5t7fQLlQ_lDfD0A7jwNNeD24 - enjoy
+//7399661668:AAH512E6YqtisFIRri1jKHky0wHXnOXwcJ8 - NDD webstudio
+
+// UNCOMMENT
+const bot = new TelegramBot("6919538601:AAH9FMpcH0ExW8jvIQIDti5TJP0MrfZu-GY", {
   polling: true,
 });
+
+const botWebstudio = new TelegramBot(
+  "7399661668:AAH512E6YqtisFIRri1jKHky0wHXnOXwcJ8",
+  {
+    polling: true,
+  }
+);
 
 const chatId = 392576703;
 
 // Function to send a message to Telegram
-function sendMessageToTelegram(message) {
+function sendMessageToTelegram(bot, message) {
   bot
     .sendMessage(chatId, message)
     .then(() => console.log("Message sent to Telegram successfully"))
@@ -36,16 +48,11 @@ function transformToObject(array) {
   return obj;
 }
 
-// Listen for incoming messages
+//UNCOMMENT
+// // Listen for incoming messages
 // bot.on("message", (msg) => {
-//   // Log the entire message object to find the chat ID
 //   console.log("Received message:", msg);
-
-//   // Extract the chat ID from the message object
 //   const chatId = msg.chat.id;
-
-//   // Now you can use the chat ID to send messages to this chat
-//   // For example:
 //   sendMessageToTelegram("Your chat ID is: " + chatId);
 // });
 
@@ -81,13 +88,59 @@ const server = http.createServer((req, res) => {
       Доп сообщение : ${result.additionalMessage}
       `;
 
-        sendMessageToTelegram(template);
+        sendMessageToTelegram(bot, template);
       }
 
       // Optionally, you can process the form data here
 
       res.writeHead(200, { "Content-Type": "text/plain" });
       res.end("Form data received and stored on the server. Thank you!");
+    });
+  } else if (req.method === "POST" && req.url === "/submit_webstudio") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const parsedBody = qs.parse(body);
+
+      // Get the stringified JSON object from the object's key
+      const jsonString = Object.keys(parsedBody)[0];
+
+      // Parse the JSON string into an object
+      const bodyObject = JSON.parse(jsonString);
+
+      // Access fields inside the parsed body object
+      const name = bodyObject.name;
+      const email = bodyObject.email;
+      const countryCode = bodyObject.countryCode;
+      const phone = bodyObject.phone;
+      const category = bodyObject.category;
+      const comments = bodyObject.comments;
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Received your data successfully!" }));
+
+      let template = `
+
+      Имя : ${bodyObject.name}
+      Емейл : ${bodyObject.email}
+      Tел : ${bodyObject.countryCode} ${bodyObject.phone}
+      Причина : ${bodyObject.category}
+      Доп сообщение : ${bodyObject.comments}
+      `;
+
+      sendMessageToTelegram(botWebstudio, template);
+
+      // You can then use these values as needed
+      console.log("Name:", name);
+      console.log("Email:", email);
+      console.log("Country Code:", countryCode);
+      console.log("Phone:", phone);
+      console.log("Category:", category);
+      console.log("Comments:", comments);
     });
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
